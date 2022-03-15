213e9be5a6b08f8329d039747468fc73b79770b6
Ankur <anandraj2816@gmail.com>
2022-03-15 23:02:01 +0530

    Modified so that it can handle exceptions

--- main.py
+++ main.py
@@ -23,8 +23,21 @@ for (index, row) in student_data_frame.iterrows():
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 """
-user_input = str(input("Enter the word to convert to NATO alphabet:"))
 
+def get_input():
+    u_input = str(input("Enter the word to convert to NATO alphabet:")).lower()
+    try:
+        for u in u_input:
+            if u < 'a' or u > 'z':
+                raise FileNotFoundError
+    except FileNotFoundError:
+        return get_input()
+    else:
+        return u_input
+
+
+user_input = get_input()
+print(user_input)
 nato_dic = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dic.columns = ["word", "value"]
 # for w in user_input:
